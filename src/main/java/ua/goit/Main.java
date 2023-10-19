c510fad4db74999451be48c0d2312959e0bea925
Yevhenii Mitko <109312584+MitkoYevhenii@users.noreply.github.com>
2023-10-19 21:43:22 +0300

    biba

--- src/main/java/ua/goit/Main.java
+++ src/main/java/ua/goit/Main.java
@@ -6,7 +6,6 @@ import ua.goit.FrontEnd.StartFrame;
 public class Main {
     public static void main(String[] args) {
         StartFrame frame = new StartFrame();
-//        GameFrame gameFrame = new GameFrame();
-//        System.out.println(gameFrame.hashCode());
+//        biba-boba
     }
 }
